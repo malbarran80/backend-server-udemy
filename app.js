c9e76b6eaa2f4d93de598a34007415506e2d4f45
Albarrán Moreno Miguel <10599779@thyssenkrupp.gtk.local>
2020-04-29 17:44:36 +0200

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extender: false }));
+app.use(bodyParser.json())
+
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -14,14 +24,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 })
 
 // Rutas
-app.get('/', (req, res, next) => {
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    })
-
-});
 
 // Escuchar peticiones
 app.listen(3000, () => {
